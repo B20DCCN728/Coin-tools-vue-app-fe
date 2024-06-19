49487e5b92c4cde6db093b8565d58f394c19475b
B20DCCN728 <vietjohnny810@gmail.com>
2024-06-19 15:20:54 +0700

    update api call task

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <p>Batch Associtate, transfer, deploy, mint nft, check balance</p>
     <a-row :gutter="16">
       <a-col :span="12">
-        <Input />
+        <Input @data-changed="handleTerminal"></Input>
       </a-col>
       <a-col :span="12">
         <a-textarea v-model:value="value" placeholder="Paste or Insert your Privatekey List" :rows="4" />
@@ -17,6 +17,7 @@
       <a-button type="primary" loading>Primary</a-button>
     </a-space>
     <Terminal
+      :ref="terminalRef"
       welcomeMessage="Welcome to Ph Plus"
       prompt="Dead-$"
       aria-label="Ph Terminal Service"
@@ -27,8 +28,23 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import Terminal from 'primevue/terminal';
 import TerminalService from 'primevue/terminalservice'
 import Input from './components/transfer/Input.vue';
+
+const handleTerminal = (result) => {
+  console.log('Received data:', result.receivedAddress);
+  let response = `Transaction from ${result.receivedAddress} is ${result.status}!`;
+  TerminalService.emit('response', response);
+};
+const terminalRef = ref(null);  
+onMounted(() => {
+  // Example response message when terminal mounts
+  const responseMessage = "Terminal is ready.";
+
+  // Emit the response message to be displayed in the terminal
+  TerminalService.emit('response', responseMessage);
+});
+
 </script>
--- src/components/transfer/Input.vue
+++ src/components/transfer/Input.vue
@@ -1,21 +1,64 @@
 <template>
-    <div class="components-input-demo-presuffix">
-      <a-input v-model:value="userName" placeholder="Select or Insert your token address">
-        <template #prefix>
-          <user-outlined />
-        </template>
-        <template #suffix>
-          <a-tooltip title="Extra information">
-            <info-circle-outlined style="color: rgba(0, 0, 0, 0.45)" />
-          </a-tooltip>
-        </template>
-      </a-input>
-      <br />
-      <br />
-      <a-input prefix="$" suffix="HBAR" placeholder="Insert your bitch"/>
-    </div>
-  </template>
-  <script setup>
-    import { ref } from 'vue';
-    const userName = ref('');
-  </script>
+  <div class="components-input-demo-presuffix">
+    <a-input v-model:value="userName" placeholder="Select or Insert your token address">
+      <template #prefix>
+        <user-outlined />
+      </template>
+      <template #suffix>
+        <a-tooltip title="Extra information">
+          <info-circle-outlined style="color: rgba(0, 0, 0, 0.45)" />
+        </a-tooltip>
+      </template>
+    </a-input>
+    <br />
+    <br />
+    <a-input prefix="$" suffix="HBAR" placeholder="Insert your bitch"/>
+  </div>
+  <h1>
+    {{ serverData.receivedAddress }}
+  </h1>
+</template>
+<script setup>
+import { ref, onMounted, computed, watch, defineEmits } from 'vue';
+import SockJS from 'sockjs-client';
+import TerminalService from 'primevue/terminalservice'
+import Stomp from 'stompjs';
+
+const userName = ref('');
+const serverData = ref({
+  "receivedAddress": "Hello",
+  "status": "SUCCESS"
+});
+
+const emit = defineEmits(['data-changed']);
+
+// Set up the WebSocket connection
+const socketUrl = "http://localhost:8080/ws"; 
+const socket = new SockJS(socketUrl);
+const stompClient = Stomp.over(socket);
+
+// Callback function to handle received data
+const onDataReceived = (message) => {
+  const data = JSON.parse(message.body);
+  serverData.value = data;
+  console.log('Received data:', data);
+};
+
+// Watch server data to change color of value
+watch(serverData, (newServerData) => {
+  console.log(newServerData);
+  emit('data-changed', newServerData);
+});
+
+// Subscribe to a specific STOMP destination to get real-time data
+onMounted(async () => {
+    console.log('Connecting to WebSocket...');
+    stompClient.connect({}, (frame) => {
+    console.log('Connected to WebSocket');
+        // Subscribe to a specific STOMP destination to get real-time data
+    const subscription = stompClient.subscribe('/topic/transfer', onDataReceived);
+        // subscription.unsubscribe();
+    // loading.value = false;
+  });
+});
+</script>
